f27dbec14b7871a1606696e6d92320b4f5cef3f7
HOWEYSUN <850732654@qq.com>
2019-07-17 02:47:05 +0000

    基础页面结构

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,4 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-
-</html>
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Howey</a> 设计和编码
+        </p>
+        <p class="float-right">
+            <a href="mailto:name@email.com">联系我们</a>
+        </p>
+    </div>
+</footer>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,4 +1,22 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+    <div class="container">
+        <!-- Branding Image -->
+        <a class="navbar-brand " href="{{ url('/') }}">
+            LaraBBS
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-</html>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto"></ul>
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
