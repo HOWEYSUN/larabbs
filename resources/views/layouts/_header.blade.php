7f7e91933116a74cb2c4ad4dc3906168b12a7f4b
HOWEYSUN <850732654@qq.com>
2019-08-14 08:46:04 +0000

    用户可以创建话题

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -21,26 +21,31 @@
             <ul class="navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @guest
-                <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
                 @else
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
-                        <img class="mr-3" src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->name }}" width=32>
-                        {{ Auth::user()->name }}
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}"><i class="far fa-user mr-2"></i>个人中心</a>
-                        <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}"><i class="far fa-edit mr-2"></i>编辑资料</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" id="logout" href="#">
-                            <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？')">
-                                {{ csrf_field() }}
-                                <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
-                            </form>
+                    <li class="nav-item">
+                        <a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
+                            <i class="fa fa-plus"></i>
                         </a>
-                    </div>
-                </li>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+                            <img class="mr-3" src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->name }}" width=32>
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}"><i class="far fa-user mr-2"></i>个人中心</a>
+                            <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}"><i class="far fa-edit mr-2"></i>编辑资料</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" id="logout" href="#">
+                                <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？')">
+                                    {{ csrf_field() }}
+                                    <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                                </form>
+                            </a>
+                        </div>
+                    </li>
                 @endguest
             </ul>
         </div>
