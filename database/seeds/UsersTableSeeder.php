28d1c409f3e446fa601d2b6aad940de67a185ebe
HOWEYSUN <850732654@qq.com>
2019-08-19 02:55:12 +0000

    回复数据

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,12 +16,12 @@ class UsersTableSeeder extends Seeder
         $faker = app(Faker\Generator::class);
         // 头像假数据
         $avatars = [
-            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png',
-            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png',
-            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png',
-            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png',
-            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png',
-            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png',
+            'http://larabbs.test/uploads/images/avatars/201907/26/1.jpg',
+            'http://larabbs.test/uploads/images/avatars/201907/26/2.jpg',
+            'http://larabbs.test/uploads/images/avatars/201907/26/3.jpg',
+            'http://larabbs.test/uploads/images/avatars/201907/26/4.jpg',
+            'http://larabbs.test/uploads/images/avatars/201907/26/5.jpg',
+            'http://larabbs.test/uploads/images/avatars/201907/26/6.jpg',
         ];
         // 生成数据集合
         $users = factory(User::class) ->times(10) ->make() ->each(function ($user, $index)
@@ -36,7 +36,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Howey';
         $user->email = '850732654@qq.com';
-        $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->avatar = 'http://larabbs.test/uploads/images/avatars/201907/26/1.jpg';
         $user->save();
     }
 }
